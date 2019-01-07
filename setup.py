802bcd654fcd75fc52ba88fedd8a0da2b813c7ec
user <user@HKG-020-1260A.neo-derm.local>
2019-01-07 11:02:08 +0800

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='wes_sklearn_ext',
+      version='0.1',
+      description='My own sklearn extensions',
+      url='https://github.com/wesleyklhk/my_own_sklearn_extensions',
+      author='Wesley Lau',
+      author_email='wesleyklhk@yahoo.com.hk',
+      license=None,
+      packages=['wes_sklearn_ext'],
+      zip_safe=False)
